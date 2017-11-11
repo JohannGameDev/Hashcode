f723e4b1f47cc29a72cece96227c8c963f266859
Justin Kohn <justin-kohn@web.de>
2017-11-11 11:08:19 +0100

    dynamic file input

--- 2018/main.py
+++ 2018/main.py
@@ -1,4 +1,14 @@
 import reader
 import sys
 
-print (reader.split (str(sys.argv [1])))
+try:
+    file = open('filename.txt')
+    inputfile = file.readline ()
+    file.close()
+except IOError as e:
+    print ("No input file existing!")
+    filename = input ("Name of inputfile: ")
+    file = open("filename.txt", "w")
+    file.write (filename)
+
+print (reader.split (filename))
